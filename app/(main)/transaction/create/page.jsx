d5071971e7f80fa10d05623b9b23db46067c4092
Aditya-Tiwa <at424575@gmail.com>
2025-10-16 00:10:57 +0530

    Fix imports, searchParams usage, and account actions

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,32 +1,31 @@
 import { getUserAccounts } from "@/actions/dashboard";
 import { defaultCategories } from "@/data/categories";
-import React from "react";
-import AddTransactionForm from "../_components/transaction-form";
+
 import { getTransaction } from "@/actions/transaction";
+import AddTransactionForm from "../_components/transaction-form";
 
-const AddTransactionPage = async ({searchParams})=>{
-  const accounts = await getUserAccounts(); 
-
+export default async function AddTransactionPage({ searchParams }) {
   const editId = searchParams?.edit;
+  const accounts = await getUserAccounts();
+  
 
-  let initialData  = null;
-
-  if(editId){
+  let initialData = null;
+  if (editId) {
     const transaction = await getTransaction(editId);
     initialData = transaction;
   }
-  return(
-    <div className = "max-w-3xl mx-auto px-5">
-      <h1 className = "text-5xl gradient-title mb-8">{editId?"Edit":"Add"} Transaction</h1>
 
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+      </div>
       <AddTransactionForm
-      accounts = {accounts} 
-      categories = {defaultCategories}
-      editMode = {!!editId}
-      initialDate = {initialData}
+        accounts={accounts}
+        categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
       />
     </div>
-  )
+  );
 }
-
-export default AddTransactionPage;
